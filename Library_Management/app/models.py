1c65be6122b5ec8bfd5c8dcb1748df629d0109ad
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-02 01:30:18 +0530

    book and borrower models added

--- Library_Management/app/models.py
+++ Library_Management/app/models.py
@@ -7,3 +7,24 @@ class Author(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author, related_name='books')
+    isbn = models.CharField(max_length=13)
+    borrower = models.OneToOneField('Borrower', on_delete=models.SET_NULL, null=True, blank=True)
+    #borror , on delete : deletes model when field is null
+    additional_info = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Borrower(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
