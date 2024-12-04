57ec7eff876b18f23b2244899a9248c5ee48e6c8
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-04 13:28:08 +0530

    added admin customizations

--- Library_Management/app/admin.py
+++ Library_Management/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Author
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display =('first_name', 'last_name' , 'date_of_birth')  #display these columns
+    search_fields =('first_name', 'last_name')  #search by these fields
+    list_filter= ('date_of_birth',)  #filter by date of birth
+
