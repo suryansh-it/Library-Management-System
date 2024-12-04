5efc81494830dc06fbfa09f788daaa381d21e1d3
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-04 13:34:56 +0530

    added admin customizations for book

--- Library_Management/app/admin.py
+++ Library_Management/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Author
+from .models import Book
+from .models import Borrower
 # Register your models here.
 
 @admin.register(Author)
@@ -8,3 +10,11 @@ class AuthorAdmin(admin.ModelAdmin):
     search_fields =('first_name', 'last_name')  #search by these fields
     list_filter= ('date_of_birth',)  #filter by date of birth
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display =('title', 'authors' , 'idbn','borrower')  
+    search_fields =('title', 'isbn')  
+    list_filter= ('borrower',)
+
+@admin.register(Borrower)
+class BorrowerAdmin(admin.ModelAdmin)
