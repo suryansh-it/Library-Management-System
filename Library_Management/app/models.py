42dff587e93e135b39c783691302ffc05a2101c2
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-06 12:45:39 +0530

    comments added to models

--- Library_Management/app/models.py
+++ Library_Management/app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-class Author(models.Model):
+# Custom Manager to filter available books
+# class AvailableBookManager(models.Manager):  # Custom model manager
+#     def get_queryset(self):
+#         return super().get_queryset().filter(is_available=True)
+
+
+class Author(models.Model):             # Many-to-Many relationship with Book
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
@@ -11,21 +17,21 @@ class Author(models.Model):
 
 
 
-class Book(models.Model):
+class Book(models.Model):                  # One-to-One: Borrower | Many-to-Many: Author
     title = models.CharField(max_length=200)
-    authors = models.ManyToManyField(Author, related_name='books')
+    authors = models.ManyToManyField(Author, related_name='books')   # Many-to-Many relationship
     isbn = models.CharField(max_length=13)
-    borrower = models.OneToOneField('Borrower', on_delete=models.SET_NULL, null=True, blank=True)
+    borrower = models.OneToOneField('Borrower', on_delete=models.SET_NULL, null=True, blank=True)   # One-to-One relationship
     #borror , on delete : deletes model when field is null
     additional_info = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.title  #return title when the obj is printed
     
-    is_available = models.BooleanField(default=True)
+    is_available = models.BooleanField(default=True)     # Boolean to track availability
 
 
-class Borrower(models.Model):
+class Borrower(models.Model):           # One-to-Many with Book (via borrower field in Book)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
