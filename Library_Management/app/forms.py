0bbe2403ed24b64717785309e225fef55975bdfd
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-06 13:01:07 +0530

    added forms for book and isbn

--- Library_Management/app/forms.py
+++ Library_Management/app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Book
 
+# Form to handle book data
 class BookForm(forms.ModelForm):
     class Meta:
         model=Book
         fields= ['title', 'authors', 'isbn', 'borrower', 'is_available', 'additional_info']
+
+    
+    # Custom validation for ISBN field
+    def clean_isbn(self):  # Form validation
+        isbn = self.cleaned_data.get('isbn')
+        if len(isbn) != 13:
+            raise forms.ValidationError('ISBN must be exactly 13 characters long.')
+        return isbn
