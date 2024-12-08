923a93e00b307923a658f85bbb67d0b44e9f81ca
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-08 22:20:00 +0530

    user registration form set up

--- Library_Management/app/forms.py
+++ Library_Management/app/forms.py
@@ -24,3 +24,17 @@ class BookForm(forms.ModelForm):
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):  # Validate passwords
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+        return cleaned_data
