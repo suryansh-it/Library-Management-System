1b7b9022f5e82d9e1dfef7bc3ee8c8ad717355c8
Suryansh Sharma <suryansharma09@gmail.com>
2024-12-08 22:18:49 +0530

    added user registration form

--- Library_Management/app/forms.py
+++ Library_Management/app/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Book
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+
 
 # Form to handle book data
 class BookForm(forms.ModelForm):
@@ -14,3 +18,9 @@ class BookForm(forms.ModelForm):
         if len(isbn) != 13:
             raise forms.ValidationError('ISBN must be exactly 13 characters long.')
         return isbn
+    
+
+# User registration form
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
